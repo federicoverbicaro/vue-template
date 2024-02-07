00c65e1da35af9861fa79d38fe89d861973686f7
federicoverbicaro <federico.verbicaro@icloud.com>
2024-02-07 17:04:44 +0100

    Primo commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+ <!-- Contenuto del componente radice -->
+
+ <!-- <HelloWorld/> tag inserito nel template per richimare il componente e visualizzarlo nel dom  -->
+
+</template>
+
+<script>
+// Definizione delle opzioni del componente
+// import HelloWorld from './components/HelloWorld.vue'; esempio di importazioni di un componente 
+
+
+//inserimento tag creazione componenti 
+export default {
+
+  components : {
+    // HelloWorld //esempio di richiamo del componente all'iterno di components
+}
+  
+}
+
+</script>
+
+
+<style lang="scss">
+/* Stili del componente radice */
+@use "../src/styles/general.scss" //collegamento a file general.scss per utilizzare styles
+</style>
